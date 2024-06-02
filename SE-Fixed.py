add596972160084ab984bfc9b8f8d27d32ce4f3f
namlyf <168154124+namlyf@users.noreply.github.com>
2024-06-02 22:36:52 +0700

    Update SE-Fixed.py

--- SE-Fixed.py
+++ SE-Fixed.py
@@ -1,147 +1,135 @@
 from pulp import *
 import networkx as nx
-import matplotlib.pyplot as plt
+import pulp as pl
 
 # Define G
 # G = 5 nodes 8 links
-G = nx.DiGraph()
 
-for i in range(5):
-    G.add_node(i, a=10)
+#Definition of Graph
+def slice_generate():
+    G = nx.DiGraph()
 
-for i in range(4):
-    G.add_edge(i, i + 1, a=10)
-    G.add_edge(i + 1, i, a=10)
+    for i in range(5):
+        G.add_node(i, a=10)
+
+    for i in range(4):
+        G.add_edge(i, i + 1, a=10)
+        G.add_edge(i + 1, i, a=10)
+
+    return G
 
 # Define GS
 # GS = 3 nodes 2 link
 
-SFCs = nx.DiGraph()
+PHY=nx.DiGraph()
+for i in range(5):
+    PHY.add_node(i,a=10)
+for i in range(4):
+    PHY.add_edge(i, i+1, a=10)
+    PHY.add_edge(i+1, i, a=10)
 
-SFCs.add_node(0, r=2)
-SFCs.add_node(1, r=2)
-SFCs.add_node(2, r=2)
-
-SFCs.add_edge(0, 1, r=5)
-SFCs.add_edge(1, 2, r=5)
+S = list()
+for i in range(5):
+    S.append(slice_generate())
 
 # input
-G
-SFCs
-
+PHY
+S
 # G, K{SFC}
 
 # ILP
-problem = pulp.LpProblem(name="graph-maaping", sense=pulp.LpMaximize)
+problem = pulp.LpProblem(name="graph-maaping", sense=LpMaximize)
 
-xNode = list()
-for sfc in SFCs:
-    xNode.append(
-        pulp.LpVariable.dicts(
-            name=f"xNode{SFCs.index(sfc)}",
-            indices=(sfc.nodes, G.nodes),
-            cat="Binary"
-        )
-    )
-
-xEdge = list()
-for sfc in SFCs:
-    xEdge.append(
-        pulp.LpVariable.dicts(
-            name=f"xEdge{SFCs.index(sfc)}",
-            indices=(sfc.edges, G.edges),
-            cat="Binary"
-        )
-    )
+#variables
+xNode = pl.LpVariable.dicts(name="xNode",
+                              indices= [(i,v,s) for i in PHY.nodes for s in range(len(S)) for v in S[s].nodes],
+                              cat=LpBinary)
 
 
-pi = pulp.LpVariable(
-    name="pi",
-    indices=(range(len(SFCs))),
-    cat=pulp.LpBinary
-)
+xEdge = pl.LpVariable.dicts(name="xEdge",
+                            indices=[(i,w,v,j,s)
+                               for i,j in PHY.edges
+                               for s in range(len(S))
+                               for w,v in S[s].edges],
+                            cat=LpBinary)
 
-aNode = nx.get_node_attributes(G, "a")
-aEdge = nx.get_edge_attributes(G, "a")
 
-rNode = nx.get_node_attributes(SFCs, "r")
-rEdge = nx.get_edge_attributes(SFCs, "r")
+
+
+pi = pulp.LpVariable( name="pi",cat=LpBinary)
+
+
+aNode = nx.get_node_attributes(PHY, "a")
+aEdge = nx.get_edge_attributes(PHY, "a")
+
+for s in range(len(S)):
+    rNode = nx.get_node_attributes(S[s], "r")
+    rEdge = nx.get_edge_attributes(S[s], "r")
 
 # C1
-
-for i in G.nodes:
-    problem += (
-        pulp.lpSum(
-            pulp.lpSum(
-                xNode[SFCs.index(sfc)][v][i] * rNode[v]
-                for v in SFCs.nodes
-            )
-            for sfc in SFCs
-        ) <= aNode[i],
-        f"C1_{i}"
-    )
+for i in PHY.nodes:
+    problem += pulp.lpSum(
+                    xNode[(i,v,s)] * rNode[v] for s in range(len(S)) for v in S[s].nodes
+                ) <= aNode[i], f"C1_{i}"
 
 # C2
 
-for ij in G.edges:
+for i,j in PHY.edges:                                           
     problem += (
         pulp.lpSum(
-            pulp.lpSum(
-                xEdge[SFCs.index(sfc)][vw][ij] * rEdge[vw]
-                for vw in SFCs.edges
+                xEdge[(i,j,v,w,s)] *rNode[(v,w)] for s in range(len(S)) for (v,w) in S[s].edges
             )
-            for sfc in SFCs
-        ) <= aEdge[ij],
-        f"C2_{ij}"
-    )
+         <= aEdge[(i,j)],
+       f,"C2_{i}_{j}"
+       )
 
 # C3
-
-for sfc in SFCs:
-    for i in G.nodes:
+    for i in PHY.nodes:
         problem += (
             pulp.lpSum(
-                xNode[SFCs.index(sfc)][v][i]
-                for v in SFCs.nodes
-            ) <= 1,
-            f"C3_{i}_{SFCs.index(sfc)}"
+               xNode[(s,v,i)] for v in S[s].nodes for s in range(len(S))
+            ) <= pi[s],
+            f"C3_{i}"
         )
 
 # C4
-
-for sfc in SFCs:
-    for v in SFCs.nodes:
+for s in range(len(S)):
+    for v in S[s].nodes:
         problem += (
             pulp.lpSum(
-                xNode[SFCs.index(sfc)][v][i]
-                for i in G.nodes
-            ) == pi[SFCs.index(sfc)],
-            f"C4_{v}_{SFCs.index(sfc)}"
+                xNode[(s,v,i)] for i in PHY.nodes
+            ) == pi[s],
+            f"C4_{s}_{v}"
+
         )
 
 # C5
+for s in range(len(S)):
+    for w,v in S[s].edges:
+        for i in PHY.nodes:
+            for j in PHY.nodes:
+                if i != j:
+                   if (s,w,v,i,j) in xEdge and (s,w,v,j,i) in xEdge:
+                       problem += (
+                           pulp.lpSum(
+                               xEdge[(s,w,v,i,j)]
+                           ) - pulp.lpSum(
+                               xEdge[(s,w,v,j,i)]
+                           ) == xNode[(s,v,i)] - xNode[(s,w,i)],
+                           f"C5_{s}_{w}_{v}_{i}_{j}"
+                       )
 
-for sfc in SFCs:
-    for vw in SFCs.edges:
-        for i in G.nodes:
-            problem += (
-                pulp.lpSum(
-                    xEdge[SFCs.index(sfc)][vw].get(i, j)
-                    for j in G.nodes
-                )
-                -
-                pulp.lpSum(
-                    xEdge[SFCs.index(sfc)][vw].get(j, i)
-                    for j in G.nodes
-                ) == xNode[SFCs.index(sfc)][vw[0]][i] - xNode[SFCs.index(sfc)][vw[1]][i],
-                f"C5_{vw}_{i}_{SFCs.index(sfc)}"
-            )
-
+# Objective function
 problem += (
-    pulp.lpSum(
-        xNode[v][i]
-        for v in SFCs.nodes
-        for i in G.nodes
+    pulp.lpSum(pi[s] for s in range(len(S)))
     )
-)
 print(problem)
+
+#solve problem
+result=problem.solve()
+print(pulp.LpStatus[result])
+
+# Output
+for v in problem.variables():
+    print(v.name, "=", v.varValue)
+
